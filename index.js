a66eb1604cb00ffdc4ef0cec6244c6d0ebf403d0
+ <+>
2025-01-01 22:57:38 +0300

    adds

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return`This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity="go to the office"){
+    return`This Monday, I will ${activity}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(flair='*'){
+    return function(adjective='a hard worker'){
+        return`You are ${flair}${adjective}${flair}!`;
+    };
+}
+const encouragingPromptFunction=wrapAdjective('||');
+console.log(encouragingPromptFunction("a dedicated programmer"));
+console.log(encouragingPromptFunction());
